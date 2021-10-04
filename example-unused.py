9ef9a9a7e9d9bf4f4b6aabab9f85e9abb867cb6e
Kipras <kipraszapas@gmail.com>
2021-10-03 20:53:06 -0400

    v1.3

--- example-unused.py
+++
@@ -1,8 +0,0 @@
-from brscr import brainly
-from unidecode import unidecode
-
-scrap=brainly("Simplify: −3(4x + 5)(2x + 4)", 1)
-for i in scrap:
-	print(unidecode(i.question.content))
-	for answer in i.answers:
-		print(unidecode(answer.content))
